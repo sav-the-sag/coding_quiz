bf49ca5f0f0feccbd2ecb014b7d8f075b73682a7
savan <savannahwilliams243@gmail.com>
2023-10-16 16:44:57 -0700

    created js file and linked to index.html&quiz.html

---
+++ assets/quiz.js
@@ -0,0 +1,16 @@
+// add a variable for questions //
+
+var questions = [
+    {
+        id: 1,
+        question: "What does HTML stand for?",
+        answer: "Hypertext Markup Language",
+        option: [
+            "Holy Trinity Meat Locker",
+            "Hypertext Markup Langauge",
+            "Hypertext Makeup Language"
+        ]
+    },
+
+
+]
