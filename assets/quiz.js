61e3b341369d2f3c215682b9ecbdc32b1431225b
savan <savannahwilliams243@gmail.com>
2023-10-16 18:35:45 -0700

    added another html file for high scores

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,3 +1,27 @@
+// make a variable for each option //
+
+var option1 = document.querySelector("#option1")
+
+
+var currentIndex = 0
+
+// add a timer //
+var timeEl = document.querySelector(".time");
+var secondsLeft = 60;
+
+function setTime() {
+  // Set interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
 // add a variable for questions //
 
 var questions = [
@@ -14,3 +38,4 @@ var questions = [
 
 
 ]
+
